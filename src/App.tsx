68e31502a9268b6fb4c94136b6adbe71a6127777
Simon <sillen.00@hotmail.com>
2023-08-25 13:53:24 +0200

    new route and added image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,16 @@ import {
 } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import Home from "./pages/Home";
 import Coworker from "./pages/Coworker";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
-      <Route path="/shop" element={<Coworker />} />
+      <Route path="/coworker" element={<Coworker />} />
+      <Route path="/login" element={<Login />} />
     </Route>
   )
 );
