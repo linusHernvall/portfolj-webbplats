15c62b0c4ab4509017a586e935432c7aa7395c2f
Simon <sillen.00@hotmail.com>
2023-08-28 16:17:21 +0200

    removed map temperary.

--- src/components/MapSection.tsx
+++ src/components/MapSection.tsx
@@ -7,12 +7,12 @@ function MapSection() {
   return (
     <>
       <div style={{ backgroundColor: "orange" }}>Map Section</div>
-      <iframe
+      {/* <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3689.668600788638!2d11.9794730116044!3d57.70908028525506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff343416e21cd%3A0xb0a5581d250fd67e!2sG%C3%B6teborg%20Centralstation!5e0!3m2!1ssv!2sse!4v1693230865220!5m2!1ssv!2sse"
         width="600"
         height="450"
         loading="lazy"
-      ></iframe>
+      ></iframe> */}
     </>
   );
 }
