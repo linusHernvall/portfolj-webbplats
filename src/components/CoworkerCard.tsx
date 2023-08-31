81aea74060617cd2c96df422e15aa6e20edcbeeb
Simon <sillen.00@hotmail.com>
2023-08-31 17:55:20 +0200

    start på team korten.

--- src/components/CoworkerCard.tsx
+++ src/components/CoworkerCard.tsx
@@ -1,3 +1,5 @@
+import { Avatar, Button, Paper, Text } from '@mantine/core';
+
 function CoworkerCard(props: {
   name: string;
   expertise: string;
@@ -12,15 +14,35 @@ function CoworkerCard(props: {
 
   // TSX ------------------------------------------------------------------------
   return (
-    <div>
-      <div style={{ border: "2px solid red" }}>
-        <h3>{name}</h3>
-        <h5>{expertise}</h5>
-        <p>{phone}</p>
-        <p>{email}</p>
-        <p>{bio}</p>
-      </div>
-    </div>
+    // <div>
+    //   <div style={{ border: "2px solid red" }}>
+    //     <h3>{name}</h3>
+    //     <h5>{expertise}</h5>
+    //     <p>{phone}</p>
+    //     <p>{email}</p>
+    //     <p>{bio}</p>
+    //   </div>
+    // </div>
+    <Paper
+      radius='md'
+      withBorder
+      p='lg'
+      sx={theme => ({
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+      })}
+    >
+      <Avatar src={phone} size={120} radius={120} mx='auto' />
+      <Text ta='center' fz='lg' weight={500} mt='md'>
+        {name}
+      </Text>
+      <Text ta='center' c='dimmed' fz='sm'>
+        {bio} {expertise} {email}
+      </Text>
+
+      <Button variant='default' fullWidth mt='md'>
+        Send message
+      </Button>
+    </Paper>
   );
 }
 
