97ef1a36f656bca93f3a8106ef30706e74740cde
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-08-31 18:24:06 +0200

    add stack svgs and implement to marquee

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -8,12 +8,25 @@ function TechStack() {
   // TSX ------------------------------------------------------------------------
   return (
     <>
-      <Marquee speed={25} autoFill>
-        <img height='40px' style={{ marginRight: '2rem' }} src='/public/react-logo.png' />
-        <img height='40px' style={{ marginRight: '2rem' }} src='/public/typeScript-logo.png' />
-        <img height='40px' style={{ marginRight: '2rem' }} src='/public/angular-logo.png' />
-        <img height='40px' style={{ marginRight: '2rem' }} src='/public/mantine-logo.png' />
-        <img height='40px' style={{ marginRight: '2rem' }} src='/public/vue-logo.png' />
+      <Marquee speed={25}>
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/html-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/css-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/javascript-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/typescript-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/bootstrap-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/angular-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/nodejs-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/nodemon-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/c-sharp-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/python-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/java-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/php-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/ruby-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/vue-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/figma-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/photoshop-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/vite-icon.svg' />
+        <img height='40px' style={{ marginRight: '2.3rem' }} src='/public/cypress-icon.svg' />
       </Marquee>
     </>
   );
