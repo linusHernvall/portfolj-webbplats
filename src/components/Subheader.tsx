fdb8dd10b2c3368496ffb8a1a163514d2a925420
Simon <sillen.00@hotmail.com>
2023-08-31 17:32:31 +0200

    New image and padding on right side of text.

--- src/components/Subheader.tsx
+++ src/components/Subheader.tsx
@@ -14,27 +14,41 @@ function Subheader() {
       <Box
         h={'60vh'}
         sx={{
-          backgroundImage: 'url("subhead-image.png")',
+          backgroundImage: 'url("/subheader.jpeg")',
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
-          backgroundPosition: '30% center',
+          backgroundPosition: 'center center',
         }}
       >
         {matches ? (
-          <Title sx={{ textShadow: 'black 4px 4px 6px' }} fz='96px' pt='25vh' ta='right' c='white'>
+          <Title
+            sx={{ textShadow: 'black 4px 4px 6px' }}
+            fz='96px'
+            pt='25vh'
+            pr='50px'
+            ta='right'
+            c='white'
+          >
             DIN WEBBYRÅ
           </Title>
         ) : (
-          <Title sx={{ textShadow: 'black 3px 3px 5px' }} fz='48px' pt='25vh' ta='right' c='white'>
+          <Title
+            sx={{ textShadow: 'black 3px 3px 5px' }}
+            fz='48px'
+            pt='25vh'
+            pr='5px'
+            ta='right'
+            c='white'
+          >
             DIN WEBBYRÅ
           </Title>
         )}
         {matches ? (
-          <Title sx={{ textShadow: 'black 3px 3px 5px' }} order={2} ta='right' c='white'>
+          <Title sx={{ textShadow: 'black 3px 3px 5px' }} order={2} ta='right' pr='50px' c='white'>
             FÖR HELTÄCKANDE IT-LÖSNINGAR
           </Title>
         ) : (
-          <Title sx={{ textShadow: 'black 2px 3px 4px' }} order={3} ta='right' c='white'>
+          <Title sx={{ textShadow: 'black 2px 3px 4px' }} order={3} ta='right' pr='5px' c='white'>
             FÖR HELTÄCKANDE IT-LÖSNINGAR
           </Title>
         )}
