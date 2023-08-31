db98d84f844cb29559883ad31df2a41adf5ff63b
lauax <lucas12alfredsson@gmail.com>
2023-08-31 14:47:57 +0200

    klart med reponsivtet

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -1,41 +1,44 @@
 import { Carousel } from '@mantine/carousel';
 import { Progress } from '@mantine/core';
+import { useMediaQuery } from 'react-responsive';
 import jacyz from '/jacyz.jpg';
 import mcdonalds from '/mcdonalds.jpg';
 import volvo from '/volvo.jpg';
 
-
-
-
 function ProjectCarousel() {
-  
+  const isSmallScreen = useMediaQuery({ maxWidth: 576 });
+  const isMediumScreen = useMediaQuery({ minWidth: 577, maxWidth: 768 });
+  const isLargeScreen = useMediaQuery({ minWidth: 769, maxWidth: 992 });
 
- 
+  const slideSize = isSmallScreen
+    ? '100vw'
+    : isMediumScreen
+    ? '75vw'
+    : isLargeScreen
+    ? '50vw'
+    : '33.33vw';
+
   return (
     <>
-    <div style={{width:'100%'}}>
-      <Carousel
-      slideSize='100%'
-      height={800} 
-      align='center'
-      controlsOffset="xs" 
-      controlSize={27} 
-      draggable={false} 
-      withIndicators
-      initialSlide={0}
-      breakpoints={[{ maxWidth: '100vw', slideSize: '100%' }]}
-      >
-        
-        <Carousel.Slide><img src={volvo}></img></Carousel.Slide>
-        <Carousel.Slide><img src={jacyz}></img></Carousel.Slide>
-        <Carousel.Slide><img src={mcdonalds}></img></Carousel.Slide>
-        
-      </Carousel>
-      </div>
-      <Progress
-        
-      />
+      
+        <Carousel
+          slideSize={slideSize}
+          height={800}
+          align='center'
+          controlsOffset="xs"
+          controlSize={27}
+          draggable={false}
+          withIndicators
+          initialSlide={0}
+        >
+          <Carousel.Slide><img style={{width:'100vw'}} src={volvo} alt="Volvo" /></Carousel.Slide>
+          <Carousel.Slide><img  style={{width:'100vw'}} src={jacyz} alt="Jacyz" /></Carousel.Slide>
+          <Carousel.Slide><img  style={{width:'100vw'}} src={mcdonalds} alt="McDonald's" /></Carousel.Slide>
+        </Carousel>
+     
+      <Progress />
     </>
   );
 }
+
 export default ProjectCarousel;
