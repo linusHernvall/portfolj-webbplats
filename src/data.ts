cf5a795f2d16ef83ff43a211dea42f77de243762
Linus Hernvall <linus.hernvall@gmail.com>
2023-09-01 18:54:42 +0200

    Added bio & techStack to coworker added react icon

--- src/data.ts
+++ src/data.ts
@@ -6,10 +6,14 @@ export const coworkers = [
     expertise: 'Frontend-utvecklare',
     phone: '0730-55 66 77',
     email: 'daniel@eldsit.se',
-    techStack: '',
+    techStack: ['/react-icon.svg', '/angular-icon.svg', '/typescript-icon.svg', '/nodejs-icon.svg'],
     shortBio:
       'Frontend-utvecklare med sinne för digitala lösningar och passion för kod samt stora intressen inom UX.',
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: `Daniel Vanpalo är den passionerade och skickliga frontend-utvecklaren som tar våra digitala visioner och omsätter dem till visuella mästerverk.
+\nMed Daniels djupa förståelse för användarbeteenden och det senaste inom webbteknologi, förvandlas koncept till gränssnitt som inte bara imponerar, utan engagerar och förbluffar besökare. Med 14 års erfarenhet i branschen har Daniel ett enastående grepp om HTML, CSS och JavaScript och är skicklig på att skapa responsiva och estetiskt tilltalande webbplatser. 
+\nMen det stannar inte där – Daniel är också en mästare på att samarbeta med designers och backend-utvecklare för att säkerställa en sömlös integration av funktion och form. Med en öppenhet för nya idéer och en förmåga att hantera utmaningar med elegans, gör Daniel processen att ta ett koncept från skissbordet till skärmen till en fantastisk resa.
+\nOavsett om det handlar om att optimera prestanda, implementera animationer eller säkerställa fullständig responsivitet, är Daniel hängiven att skapa webbplatser som inte bara ser fantastiska ut, utan också fungerar enligt de högsta standarderna.
+\nLär känna Daniel och upptäck hur han omdefinierar användarupplevelsen och tar din webbplats till en ny nivå av interaktivitet och skönhet.`,
   },
   {
     id: 2,
@@ -18,44 +22,60 @@ export const coworkers = [
     expertise: 'Frontend-utvecklare',
     phone: '0730-55 66 77',
     email: 'emil@eldsit.se',
-    techStack: '',
+    techStack: ['/react-icon.svg', '/vue-icon.svg', '/typescript-icon.svg', '/nodemon-icon.svg'],
     shortBio:
       'Frontend-superstar med sinne för effektiv kodning med smarta  lösningar och passion för tillgänglighet till internet för alla samt gillar Bitcoin.',
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: `Emil Helgesson är den passionerade och skickliga frontend-utvecklaren som tar våra digitala visioner och omsätter dem till visuella mästerverk.
+\nMed Emil's djupa förståelse för användarbeteenden och det senaste inom webbteknologi, förvandlas koncept till gränssnitt som inte bara imponerar, utan engagerar och förbluffar besökare. Med 8 års erfarenhet i branschen har Emil ett enastående grepp om HTML, CSS och JavaScript och är skicklig på att skapa responsiva och estetiskt tilltalande webbplatser.
+\nMen det stannar inte där – Emil är också en mästare på att samarbeta med designers och backend-utvecklare för att säkerställa en sömlös integration av funktion och form. Med en öppenhet för nya idéer och en förmåga att hantera utmaningar med elegans, gör Emil processen att ta ett koncept från skissbordet till skärmen till en fantastisk resa.
+\nOavsett om det handlar om att optimera prestanda, implementera animationer eller säkerställa fullständig responsivitet, är Emil hängiven att skapa webbplatser som inte bara ser fantastiska ut, utan också fungerar enligt de högsta standarderna.
+\nLär känna Emil och upptäck hur han omdefinierar användarupplevelsen och tar din webbplats till en ny nivå av interaktivitet och skönhet.`,
   },
   {
     id: 3,
     fullName: 'Simon Bengtsson',
-    imgUrl: 'simon.jpg',
+    imgUrl: '/simon.jpg',
     expertise: 'Backend-utvecklare',
     phone: '0730-55 66 77',
     email: 'simon@eldsit.se',
-    techStack: '',
+    techStack: ['/react-icon.svg', '/php-icon.svg', '/java-icon.svg', '/c-sharp-icon.svg'],
     shortBio:
       'Backend-virtuos med sinne för digitala lösningar och passion för kod samt snabba motorcyklar.',
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: `Simon Bengtsson, vår begåvade backend-utvecklare. När det kommer till att skapa robusta och effektiva digitala lösningar är Simon den som håller i trådarna och säkerställer att allt fungerar sömlöst.
+\nMed en 6 års erfarenhet inom backend-utveckling är Simon en sann expert på programmeringsspråk som C-sharp, Java och PHP. Hans förmåga att bygga och optimera databaser och hantera komplexa serverstrukturer är oöverträffad. Simon ser till att dataflöden är effektiva, säkra och skalbara – en grundläggande pelare för varje framgångsrik digital plattform.
+\nMen Simon är mer än bara en kodningsguru. Han är också en lagspelare som trivs i samarbete med frontend-utvecklare och designers för att säkerställa att användarupplevelsen är lika imponerande under huven som på skärmen. Hans analytiska sinne och problemlösningsförmåga gör honom till den perfekta partnern för att tackla utmaningar och hitta innovativa lösningar.
+\nAtt hålla sig uppdaterad om de senaste trenderna inom backend-utveckling är en självklarhet för Simon. Han tar ständigt till sig ny teknik och bästa praxis för att leverera webbplatser och applikationer som inte bara möter dagens krav, utan som också är redo att möta framtida utmaningar.
+\nLär känna Simon Bengtsson, backend-virtuosen som omdefinierar hur digitala lösningar byggs och optimeras. Med honom i vårt team kan du vara säker på att din digitala närvaro kommer att stå stadigt på en pålitlig och innovativ teknisk grund.`,
   },
   {
     id: 4,
     fullName: 'Linus Hernvall',
-    imgUrl: 'linus.jpg',
+    imgUrl: '/linus.jpg',
     expertise: 'Backend-utvecklare',
     phone: '0730-55 66 77',
     email: 'linus@eldsit.se',
-    techStack: '',
+    techStack: ['/python-icon.svg', '/nodejs-icon.svg', '/ruby-icon.svg', '/photoshop-icon.svg'],
     shortBio: 'Backend-fantom med läggning åt problemlösningar och ett stort hjärta för livemusik.',
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: `Möt Linus Hernvall, vår skicklige backend-utvecklare som är mästare på att bygga robusta och högpresterande digitala lösningar. Linus är den som tar kontrollen och ser till att allt flyter smidigt när det gäller tekniskt djupdykande.
+\nMed imponerande 10 års erfarenhet inom backend-utveckling är Linus en sann auktoritet inom programmeringsspråk som Python, NodeJS och Ruby. Hans förmåga att konstruera och optimera databaser samt hantera intrikata serverstrukturer är oöverträffad. Linus ser till att datatrafiken flyter effektivt, säkert och är redo att hantera tillväxt – en grundläggande hörnsten för varje framgångsrik digital plattform.
+\nMen Linus är mer än bara en kodmästare. Han är också en lagspelare som blomstrar i samarbete med frontend-utvecklare och designers för att garantera att användarupplevelsen är lika imponerande under huven som på skärmen. Med sitt analytiska sinne och sin förmåga att lösa problem är han den ultimata partnern när det gäller att möta utmaningar och skapa innovativa lösningar.
+\nLinus följer naturligtvis de senaste rönen inom backend-utveckling på nära håll. Han absorberar kontinuerligt ny teknik och bästa praxis för att leverera webbplatser och applikationer som inte bara uppfyller dagens krav, utan som också är rustade för att bemöta framtida utmaningar.
+\nLär känna Linus Hernvall, backend-virtuosen som omdefinierar hur digitala lösningar byggs och optimeras. Med honom i vårt team kan du vara trygg med att din digitala närvaro kommer att stå stadigt på en pålitlig och innovativ teknisk grund.`,
   },
   {
     id: 5,
     fullName: 'Lucas Alfredsson',
-    imgUrl: 'lucas.jpg',
+    imgUrl: '/lucas.jpg',
     expertise: 'UX-designer',
     phone: '0730-55 66 77',
     email: 'lucas@eldsit.se',
-    techStack: '',
+    techStack: ['/photoshop-icon.svg', '/figma-icon.svg', '/react-icon.svg', '/bootstrap-icon.svg'],
     shortBio:
       'UX Designer med ett öga för detaljer och ett ständigt intresse att besöka nya platser på vår jord.',
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: `Med en passion för att förstå människors beteenden och behov, tar Lucas våra digitala visioner och omsätter dem till sömlösa och engagerande användarflöden. Med 3 års erfarenhet i branschen kombinerar han kreativitet med strategiskt tänkande för att skapa webbplatser och applikationer som inte bara imponerar, utan också levererar verkligt värde för användarna.
+\nLucas är en mästare på att balansera designens konst med användbarhetens vetenskap. Han trivs i att samarbeta tätt med frontend- och backend-utvecklare för att säkerställa att designen inte bara är vacker utan också genomförbar.
+\nHans färdigheter sträcker sig över användarundersökningar, informationsarkitektur, trådramarbeten och prototyper, vilket resulterar i en djup förståelse för hur användarna interagerar med digitala produkter.
+\nMen Lucas är inte bara fokuserad på det här och nu – han ser framåt och integrerar de senaste trenderna inom användarupplevelsedesign för att skapa framtidssäkra lösningar. Med öppenhet för feedback och en stark strävan efter att förbättra, tar han varje projekt till nästa nivå.
+\nUpptäck hur Lucas Alfredsson förvandlar användarcentrerade koncept till verkliga upplevelser. Med honom i spetsen för designprocessen kan du vara säker på att din digitala närvaro kommer att vara så attraktiv och intuitiv som den är minnesvärd.`,
   },
 ];
