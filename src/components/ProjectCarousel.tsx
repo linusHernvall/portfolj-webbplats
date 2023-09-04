821ceb32993a5e90c516c1710e144b3b769b922d
Simon <sillen.00@hotmail.com>
2023-09-04 10:07:13 +0200

    responsive carousel

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -1,41 +1,40 @@
 import { Carousel } from '@mantine/carousel';
 import { Progress } from '@mantine/core';
-import { useMediaQuery } from 'react-responsive';
 import jacyz from '/jacyz.jpg';
 import mcdonalds from '/mcdonalds.jpg';
 import volvo from '/volvo.jpg';
 
 function ProjectCarousel() {
-  const isSmallScreen = useMediaQuery({ maxWidth: 576 });
-  const isMediumScreen = useMediaQuery({ minWidth: 577, maxWidth: 768 });
-  const isLargeScreen = useMediaQuery({ minWidth: 769, maxWidth: 992 });
-
-  const slideSize = isSmallScreen
-    ? '100vw'
-    : isMediumScreen
-    ? '75vw'
-    : isLargeScreen
-    ? '50vw'
-    : '33.33vw';
+  // const isSmallScreen = useMediaQuery({ maxWidth: 576 });
+  // const isMediumScreen = useMediaQuery({ minWidth: 577, maxWidth: 768 });
+  // const isLargeScreen = useMediaQuery({ minWidth: 769, maxWidth: 992 });
 
   return (
     <>
-      
-        <Carousel
-          slideSize={slideSize}
-          
-          align='center'
-          controlsOffset="xs"
-          controlSize={27}
-          draggable={false}
-          withIndicators
-          initialSlide={0}
-        >
-          <Carousel.Slide><img style={{width:'100vw'}} src={volvo} alt="Volvo" /></Carousel.Slide>
-          <Carousel.Slide><img  style={{width:'100vw'}} src={jacyz} alt="Jacyz" /></Carousel.Slide>
-          <Carousel.Slide><img  style={{width:'100vw'}} src={mcdonalds} alt="McDonald's" /></Carousel.Slide>
-        </Carousel>
-     
+      <Carousel
+        slideSize='100%'
+        align='center'
+        controlsOffset='xs'
+        controlSize={27}
+        draggable={false}
+        withIndicators
+        initialSlide={0}
+      >
+        <Carousel.Slide>
+          <img style={{ width: '100%', objectFit: 'scale-down' }} src={volvo} alt='Volvo' />
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <img style={{ width: '100%', objectFit: 'scale-down' }} src={jacyz} alt='Jacyz' />
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <img
+            style={{ width: '100%', objectFit: 'scale-down' }}
+            src={mcdonalds}
+            alt="McDonald's"
+          />
+        </Carousel.Slide>
+      </Carousel>
+
       <Progress />
     </>
   );
