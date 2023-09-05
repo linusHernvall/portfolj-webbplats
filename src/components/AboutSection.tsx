e5721679124911b795f5d8fa47121d19648882d3
Simon <sillen.00@hotmail.com>
2023-09-05 20:12:31 +0200

    om oss större text och accordion.

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -38,47 +38,46 @@ function AboutSection() {
             <br /> Vi är ett tajt team av fem utvecklare med T-formade kompetenser, vilket innebär
             att vi har djupgående expertis i flera områden. Vi kombinerar kunskap inom front-end,
             back-end och UI/UX design, vilket ger oss förmågan att leverera skräddarsydda,
-            helhetslösningar för varje klient.
-            <br />
-            <br /> Vi tror starkt på att varje företag, oavsett storlek eller bransch, har en unik
-            historia som förtjänar att berättas. Vårt uppdrag är att hjälpa dig berätta din historia
-            genom innovativa digitala lösningar som verkligen gör skillnad. Det kan handla om att
-            bygga en skräddarsydd webbplats, utveckla en e-handelsplattform eller maximera din
-            närvaro på sociala medier.
+            helhetslösningar för varje klient.Vi tror starkt på att varje företag, oavsett storlek eller bransch, har en unik historia
+            som förtjänar att berättas. 
+          </Text>
+          <Text sx={{ fontSize: '18px', width: matches ? '50%' : '100%' }}>
+            Vårt uppdrag är att hjälpa dig berätta din historia genom
+            innovativa digitala lösningar som verkligen gör skillnad. Det kan handla om att bygga en
+            skräddarsydd webbplats, utveckla en e-handelsplattform eller maximera din närvaro på
+            sociala medier.
             <br />
             <br /> Vår filosofi är enkel: Din digitala närvaro ska inte bara spegla ditt varumärke,
             den ska förstärka det. Vi är inte nöjda förrän vi har transformerat din vision till en
-            fulländad digital verklighet.
-          </Text>
-          <Text sx={{ fontSize: '18px', width: matches ? '50%' : '100%' }}>
-            Vi är engagerade i varje steg på vägen mot din onlineframgång, från första
-            konsultationen till den färdiga produkten och vidare in i framtiden med löpande
-            underhåll och uppdateringar. Vårt mål är att vara mer än en leverantör; vi vill vara en
-            långsiktig partner i din digitala resa.
-            <br />
-            <br /> Teknologin och de digitala plattformarna är ständigt föränderliga. Därför satsar
-            vi ständigt på fortbildning och att hålla oss uppdaterade om de senaste trenderna och
-            verktygen. Vi implementerar moderna, agila arbetsmetoder och fokuserar på transparens
-            och kommunikation, för att säkerställa att du alltid är i centrum för alla beslut och
-            förändringar som görs i ditt projekt. Vi erbjuder också analytiska tjänster som hjälper
-            dig att mäta och förstå din ROI, så att du kan se den konkreta påverkan av vårt arbete.
-            <br />
-            <br />
-            Vi förstår att varje projekt är en investering, både i tid och resurser. Det är därför
-            vi alltid strävar efter att överträffa dina förväntningar, inte bara genom att uppfylla
-            projektkraven men också genom att leverera lösningar som ger långsiktiga fördelar och
-            skapar verklig affärsnytta.
-            <br />
-            <br /> Låt oss hjälpa dig att förvandla din digitala vision till en konkret och
-            framgångsrik verklighet. Med vårt team av skickliga utvecklare och designers är vi redo
-            att ta din digitala närvaro till nästa nivå. Vi ser fram emot att starta denna spännande
-            resa tillsammans med dig.
+            fulländad digital verklighet. Vi är engagerade i varje steg på vägen mot din
+            onlineframgång, från första konsultationen till den färdiga produkten och vidare in i
+            framtiden med löpande underhåll och uppdateringar. Vårt mål är att vara mer än en
+            leverantör; vi vill vara en långsiktig partner i din digitala resa.
             <Accordion variant='filled' fz='18px' chevronPosition='left' defaultValue='closed'>
               <Accordion.Item value='customization'>
                 <Accordion.Control c='blue.9' p='0'>
                   READ MORE
                 </Accordion.Control>
-                <Accordion.Panel p='0'>text</Accordion.Panel>
+                <Accordion.Panel p='0'>
+                  Teknologin och de digitala plattformarna är ständigt föränderliga. Därför satsar
+                  vi ständigt på fortbildning och att hålla oss uppdaterade om de senaste trenderna
+                  och verktygen. Vi implementerar moderna, agila arbetsmetoder och fokuserar på
+                  transparens och kommunikation, för att säkerställa att du alltid är i centrum för
+                  alla beslut och förändringar som görs i ditt projekt. Vi erbjuder också analytiska
+                  tjänster som hjälper dig att mäta och förstå din ROI, så att du kan se den
+                  konkreta påverkan av vårt arbete.
+                  <br />
+                  <br />
+                  Vi förstår att varje projekt är en investering, både i tid och resurser. Det är
+                  därför vi alltid strävar efter att överträffa dina förväntningar, inte bara genom
+                  att uppfylla projektkraven men också genom att leverera lösningar som ger
+                  långsiktiga fördelar och skapar verklig affärsnytta.
+                  <br />
+                  <br /> Låt oss hjälpa dig att förvandla din digitala vision till en konkret och
+                  framgångsrik verklighet. Med vårt team av skickliga utvecklare och designers är vi
+                  redo att ta din digitala närvaro till nästa nivå. Vi ser fram emot att starta
+                  denna spännande resa tillsammans med dig.
+                </Accordion.Panel>
               </Accordion.Item>
             </Accordion>
           </Text>
